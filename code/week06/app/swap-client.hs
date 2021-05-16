b7a618ad89983db899344ee5bad527724043f4dd
specpro30 <specpro30@gmail.com>
2021-05-16 21:00:23 +0800

    completed draft with commentary for swap-clients.hs

--- code/week06/app/swap-client.hs
+++ code/week06/app/swap-client.hs
@@ -24,29 +24,31 @@ import Text.Read                               (readMaybe)
 
 import Week06.Oracle.PAB                       (OracleContracts)
 
-main :: IO ()
+main :: IO ()                                                      -- simple console interface to offer four functions for Offer, Retreive, Use, Funds 
 main = do
-    [i :: Int] <- map read <$> getArgs
-    uuid       <- read <$> readFile ('W' : show i ++ ".cid")
+    [i :: Int] <- map read <$> getArgs                             -- accept wallet number as command line param so console can be run with different wallets 
+    uuid       <- read <$> readFile ('W' : show i ++ ".cid")       -- read correct UUID from file 
     hSetBuffering stdout NoBuffering
     putStrLn $ "swap contract instance id for Wallet " ++ show i ++ ": " ++ show uuid
     go uuid
   where
     go :: UUID -> IO a
     go uuid = do
-        cmd <- readCommand
+        cmd <- readCommand                     -- passes command read from the console and executes the corresponding one of four endpoints below 
         case cmd of
             Offer amt -> offer uuid amt
             Retrieve  -> retrieve uuid
             Use       -> use uuid
             Funds     -> getFunds uuid
-        go uuid
+        go uuid                                                     -- loops forever 
 
     readCommand :: IO Command
     readCommand = do
         putStr "enter command (Offer amt, Retrieve, Use or Funds): "
         s <- getLine
         maybe readCommand return $ readMaybe s
+
+-- endpoints calling 
 
 data Command = Offer Integer | Retrieve | Use | Funds
     deriving (Show, Read, Eq, Ord)
@@ -55,7 +57,7 @@ getFunds :: UUID -> IO ()
 getFunds uuid = handle h $ runReq defaultHttpConfig $ do
     v <- req
         POST
-        (http "127.0.0.1" /: "api"  /: "new" /: "contract" /: "instance" /: pack (show uuid) /: "endpoint" /: "funds")
+        (http "127.0.0.1" /: "api"  /: "new" /: "contract" /: "instance" /: pack (show uuid) /: "endpoint" /: "funds") -- checks funds and tells them 
         (ReqBodyJson ())
         (Proxy :: Proxy (JsonResponse ()))
         (port 8080)
@@ -64,16 +66,18 @@ getFunds uuid = handle h $ runReq defaultHttpConfig $ do
         else do
             w <- req
                 GET
-                (http "127.0.0.1" /: "api"  /: "new" /: "contract" /: "instance" /: pack (show uuid) /: "status")
+                (http "127.0.0.1" /: "api"  /: "new" /: "contract" /: "instance" /: pack (show uuid) /: "status")  -- want information from web interface so need to use status endpoint 
                 NoReqBody
                 (Proxy :: Proxy (JsonResponse (ContractInstanceClientState OracleContracts)))
                 (port 8080)
-            liftIO $ putStrLn $ case fromJSON $ observableState $ cicCurrentState $ responseBody w of
-                Success (Last (Just f)) -> "funds: " ++ show (flattenValue f)
+            liftIO $ putStrLn $ case fromJSON $ observableState $ cicCurrentState $ responseBody w of  -- extract observable state from that 
+                Success (Last (Just f)) -> "funds: " ++ show (flattenValue f)  -- f is the value of Funds which is returned if everything goes well 
                 _                       -> "error decoding state"
   where
     h :: HttpException -> IO ()
     h _ = threadDelay 1_000_000 >> getFunds uuid
+
+-- these other ones are simpler because it doesn't require info from web 
 
 offer :: UUID -> Integer -> IO ()
 offer uuid amt = handle h $ runReq defaultHttpConfig $ do
