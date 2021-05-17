98595dbc5285dc50a5fd22a4e50895d35aecc710
specpro30 <specpro30@gmail.com>
2021-05-17 13:23:51 +0800

    completed draft with commentary for PAB.hs

--- code/week06/src/Week06/Oracle/PAB.hs
+++ code/week06/src/Week06/Oracle/PAB.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveGeneric      #-}
 
-module Week06.Oracle.PAB
-    ( OracleContracts (..)
+module Week06.Oracle.PAB        -- PAB (Plutus Application Backend) takes all the code and turns it into an executable to actually run the contracts. PAB gets deployed onto testnet or mainnet 
+    ( OracleContracts (..)      -- for the exercise we deploy to a simulated blockchain, but the process is the same for testnet / mainnet 
     ) where
 
-import           Data.Aeson                (FromJSON, ToJSON)
+import           Data.Aeson                (FromJSON, ToJSON)       -- 
 import           Data.Text.Prettyprint.Doc (Pretty (..), viaShow)
 import           GHC.Generics              (Generic)
 import           Ledger
 
 import qualified Week06.Oracle.Core        as Oracle
 
-data OracleContracts = Init | Oracle CurrencySymbol | Swap Oracle.Oracle
+data OracleContracts = Init | Oracle CurrencySymbol | Swap Oracle.Oracle   -- define the datatypes of the contracts that we eventually want to run 
+                                                                           -- Init setup the initial funds in the wallets
+                                                                           -- Oracle runs Oracle contracts with parameter of CurrencySumbol for the USDT Token
+                                                                           -- Swap parameterized by Oracle will be used to run the Swap contracts 
     deriving (Eq, Ord, Show, Generic, FromJSON, ToJSON)
 
 instance Pretty OracleContracts where
